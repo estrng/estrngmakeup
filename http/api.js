a2dc3dcadfa9f23f8144aa1956fc9f9ad05a09da
José Ivan R. de Oliveira <ivan_r.o@hotmail.com>
2021-10-26 18:47:00 -0300

    adjust: web api set.

--- http/api.js
+++ http/api.js
@@ -1,3 +1,3 @@
-/* const baseUrl = "http://makeup-api.herokuapp.com/api/v1"; */
-const baseUrl = "http://localhost:3000";
+const baseUrl = "http://makeup-api.herokuapp.com/api/v1";
+/* const baseUrl = "http://localhost:3000"; */
 export default baseUrl;
--- http/index.js
+++ http/index.js
@@ -2,16 +2,16 @@ import { fetchProducts } from "./fetch.js";
 import baseUrl from "./api.js";
 
 export function getProducts() {
-  /* return fetchProducts(`${baseUrl}/products.json`); */
-  return fetchProducts(`${baseUrl}/products`);
+  return fetchProducts(`${baseUrl}/products.json`);
+  /* return fetchProducts(`${baseUrl}/products`); */
 }
 
 export function getProductsByBrand(brand) {
-  /* return fetchProducts(`${baseUrl}/products.json?brand=${brand}`); */
-  return fetchProducts(`${baseUrl}/products?brand=${brand}`);
+  return fetchProducts(`${baseUrl}/products.json?brand=${brand}`);
+  /* return fetchProducts(`${baseUrl}/products?brand=${brand}`); */
 }
 
 export function getProductsByCategory(category) {
-  /* return fetchProducts(`${baseUrl}/products.json?category=${category}`); */
-  return fetchProducts(`${baseUrl}/products?category=${category}`);
+  return fetchProducts(`${baseUrl}/products.json?category=${category}`);
+  /* return fetchProducts(`${baseUrl}/products?category=${category}`); */
 }
